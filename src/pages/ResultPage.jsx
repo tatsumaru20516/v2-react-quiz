e65ae2e41fc87997f57a784c004abce71659e936
tatsumaru20516 <rixen71@gmail.com>
2025-07-14 17:43:19 +0900

    feat:Resultページの実装

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,5 +1,16 @@
+import { Link, useLocation } from "react-router-dom";
+import { ROUTES } from "../const";
+
 export default function ResultPage() {
+	const location = useLocation(); // QuizPageからの状態を取得
+	const totalQuestions = location.state.totalQuestions; // 総問題数
+	const correctCount = location.state.correctCount; // 正解数
+
 	return (
-		<div>ResultPage</div>
+		<>
+			<h1>ResultPage</h1>
+			<p>{`あなたの正解数は...全${totalQuestions}問中${correctCount}問でした！`}</p>
+			<Link to={ROUTES.QUIZ}>もう一度チャンレンジ！</Link>
+		</>
 	)
 }
